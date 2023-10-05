f42baeacc57bc1238b0b36a8e8adf6a0e0b6ca66
gektor558 <danil.trigub@gmail.com>
2023-10-05 17:54:58 +0200

    js fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import axios from "axios";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
-axios.defaults.headers.common["x-api-key"] = "live_kTsQxmYswvuMzk0C8MlLyJX0PmqkIFyi7b4g1w5vtnqKPQVJsXTTwWEYtJ5SrDia";
+const refs = {
+    catBreedsEl: document.querySelector('.breed-select'),
+    catInfoWrapper: document.querySelector('.cat-info'),
+    loader: document.querySelector('.loader-text'),
+    error: document.querySelector('.error'),
+    catSlimSelectWrapper: document.querySelector('.ss-main.breed-select'),
+}
+
+
+function getCatBreed(cat) {
+    return `<option value="${cat.id}">${cat.name}</option>`;
+}
+
+function renderCatsBreedsList(cats) {
+    let catsList;
+    for (let i = 0; i < cats.length; i++) {
+       catsList += getCatBreed(cats[i]);        
+    }
+    return catsList;
+}
